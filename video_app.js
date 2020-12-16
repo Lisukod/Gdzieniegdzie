625466877bb4a0705426c579e3faa582379036a3
Lisukod <45397160+Lisukod@users.noreply.github.com>
2020-12-16 16:09:19 +0100

    Poprawka styli

--- video_app.js
+++ video_app.js
@@ -6,17 +6,6 @@ const vid_05 = document.getElementById("video_05");
 const vid_06 = document.getElementById("video_06");
 let now_playing
 
-// let fadeout = setInterval(
-//     function() {
-//         if (now_playing.volume > 0) {
-//             now_playing.volume -= vol;
-//             video_id.volume += vol
-//         }
-//         else {
-//             clearInterval(fadeout);
-//         }
-//     }, interval);
-
 function playVid(video_id) {
     if (typeof(now_playing) == "undefined") {
         document.getElementById("black_box").classList.add("video-hidden")
@@ -41,20 +30,16 @@ function playVid(video_id) {
         let vol_up = 0;
         let fadeout = setInterval(
             function() {
-                console.log(vol_down)
                 now_playing.volume = vol_down / 10;
                 video_id.volume = vol_up / 10;
                 vol_down --;
                 vol_up ++;
                 if (vol_down < 0) {
-                    console.log("clear")
                     clearInterval(fadeout)
                     now_playing.pause();
                     now_playing = video_id
                 }
             }, 25);
-        console.log("zatrzymany")
-        
     }
 }
 
