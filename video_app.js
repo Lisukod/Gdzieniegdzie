ccf474d9c96fe7ce9a3f4a012262441f26eef8b2
Lisukod <45397160+Lisukod@users.noreply.github.com>
2021-02-01 02:36:57 +0100

    Play
    play btton and pause fixed

--- video_app.js
+++ video_app.js
@@ -22,7 +22,7 @@ var loop_counter = 0;
 
 function isLoaded() {
     let videoLoad = setInterval(
-        function(){
+        function() {
             for (let i = 0; i < vids.length; i++) {
                 if (vids[i] == 4) {
                     continue
@@ -31,29 +31,28 @@ function isLoaded() {
                 }
             }
             vids_loaded = true;
-        }
-    , 250)
+        }, 250)
     clearInterval(videoLoad)
 }
 
 // isLoaded()
 function playVid(video_id) {
-    if (vids_loaded == false){
+    if (vids_loaded == false) {
         isLoaded()
     }
     if (typeof(now_playing) == "undefined") {
         document.getElementById("black_box").classList.add("video-hidden")
         now_playing = video_id
         video_id.classList.toggle("video-hidden")
-    } 
-    
+    }
+
     if (now_playing == video_id) {
-        if (video_id.paused){
+        if (video_id.paused) {
             video_id.currentTime = 0
             video_id.play();
         } else {
             // video_id.pause();
-            }
+        }
     } else {
         video_id.currentTime = 0
         video_id.play();
@@ -66,8 +65,8 @@ function playVid(video_id) {
             function() {
                 now_playing.volume = vol_down / 10;
                 video_id.volume = vol_up / 10;
-                vol_down --;
-                vol_up ++;
+                vol_down--;
+                vol_up++;
                 if (vol_down < 0) {
                     clearInterval(fadeout)
                     now_playing.pause();
@@ -78,43 +77,54 @@ function playVid(video_id) {
 }
 
 function stopVid() {
-    if (typeof(now_playing) == "undefined"){
+    if (typeof(now_playing) == "undefined") {
         return
     } else {
         now_playing.pause()
     }
+    document.getElementById('play_button').setAttribute('onClick', 'continueVid()')
 }
 
 function continueVid() {
-    if (typeof(now_playing) == "undefined"){
+    if (typeof(now_playing) == "undefined") {
         return
     } else {
         now_playing.play()
     }
+    document.getElementById('play_button').setAttribute('onClick', 'playQueue()')
 }
 
 
 function playQueue() {
     let queued_vid_counter = 0;
-    let now_playing = queued_vid;
-    playVid(now_playing);
-    now_playing.addEventListener('ended', myHandler());
+    let now_playing_vid = vid_in_queue[queued_vid_counter];
+    if (typeof(now_playing_vid) == "undefined")
+        return;
+    playVid(now_playing_vid);
+    now_playing_vid.addEventListener('ended', myHandler);
+
     function myHandler() {
+        now_playing_vid.removeEventListener('ended', myHandler, true);
         queued_vid_counter++;
-        playVid(vid_in_queue[queued_vid_counter])
+        console.log(queued_vid_counter);
+        now_playing_vid = vid_in_queue[queued_vid_counter];
+        if (typeof(now_playing_vid) == "undefined")
+            return;
+        playVid(now_playing_vid);
+        now_playing_vid.addEventListener('ended', myHandler);
     }
 }
- 
+
 //Progres bar
 
 
 function addVidToBar(sent_vid, vid_num) {
     loop_counter++;
     vid_in_queue.push(sent_vid);
-    const progressBar = document.getElementById('point_container') 
+    const progressBar = document.getElementById('point_container')
 
     const newSongDiv = document.createElement("div");
-    newSongDiv.setAttribute('class','test-point vid-num-' + vid_num);
+    newSongDiv.setAttribute('class', 'test-point vid-num-' + vid_num);
     newSongDiv.setAttribute('id', loop_counter);
     progressBar.appendChild(newSongDiv);
 
@@ -129,11 +139,11 @@ function addVidToBar(sent_vid, vid_num) {
     backButton.setAttribute('onClick', 'moveBack(this.parentNode.parentNode);')
     deleteButton.setAttribute('onClick', 'removeFromBar(this.parentNode.parentNode);')
     forwardButton.setAttribute('onClick', 'moveFurther(this.parentNode.parentNode);')
-    
+
     backButton.innerHTML = '<';
     deleteButton.innerHTML = 'X';
     forwardButton.innerHTML = '>';
-    
+
     newSongSpan.appendChild(backButton);
     newSongSpan.appendChild(deleteButton);
     newSongSpan.appendChild(forwardButton);
@@ -142,8 +152,8 @@ function addVidToBar(sent_vid, vid_num) {
 function removeFromBar(elem) {
     const elem_id = elem.id;
     let next_sibling = elem.nextElementSibling;
-    while(next_sibling != null){
-        next_sibling.id = Number(next_sibling.id) -1;
+    while (next_sibling != null) {
+        next_sibling.id = Number(next_sibling.id) - 1;
         next_sibling = next_sibling.nextElementSibling;
     }
     elem.remove();
@@ -153,19 +163,19 @@ function removeFromBar(elem) {
 function moveFurther(elem) {
     const elem_id = elem.id;
     let next_sibling = elem.nextElementSibling;
-    if(next_sibling == null)
+    if (next_sibling == null)
         return;
     elem.parentNode.insertBefore(next_sibling, elem);
-    elem.id = Number(elem_id)++;
+    elem.id = Number(elem_id) ++;
     next_sibling.id = elem_id;
 }
 
-function moveBack(elem){
+function moveBack(elem) {
     const elem_id = elem.id;
     let previous_sibling = elem.previousSibling;
-    if(previous_sibling == null)
+    if (previous_sibling == null)
         return;
     elem.parentNode.insertBefore(elem, previous_sibling);
-    elem.id = Number(elem_id)--;
+    elem.id = Number(elem_id) --;
     previous_sibling.id = elem_id;
 }
